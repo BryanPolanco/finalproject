332942f44e03d9d87bfc02405fd2e64f96768513
shuying0814 <demouser@MacBook-Air-4.local>
2015-07-29 16:09:57 -0700

    login

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ import webapp2
 import jinja2
 import os
 import urllib
+from google.appengine.api import users
 from google.appengine.api import urlfetch
 from apiclient.discovery import build
 from optparse import OptionParser
@@ -32,8 +33,22 @@ DEVELOPER_KEY = "AIzaSyAK_0glv6MteAUDzFCa9vd8q0d6ETePcw0"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        greeting = ('<a href = "%s">Sign in or register</a>.' %
+            users.create_login_url('/drop'))
+        self.response.write('<html><body>%s</body></html>' % greeting)
+        template = JINJA_ENVIRONMENT.get_template('sign_in.html')
+        self.response.write(template.render())
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        # user = users.get_current_user()
+        # greeting = ('Welcome, %s! (<a href=%s>sign_out</a>)' %
+        #     (user.nickname(), users.create_logout_url('/')))
+        # self.response.write('<html><body>%s</body></html>' % greeting)
+
         youtube = build(
             YOUTUBE_API_SERVICE_NAME,
             YOUTUBE_API_VERSION,
@@ -56,7 +71,6 @@ class MainHandler(webapp2.RequestHandler):
         collection = [ ]
         x=0
         for i in range(counter):
-            logging.info("HEHEHHEHEHHEHE")
             string = urllib2.urlopen(url)
             bigdictionary = json.loads(string.read())
             picture = bigdictionary['data'][x]['link']
@@ -65,35 +79,16 @@ class MainHandler(webapp2.RequestHandler):
         logging.info(collection)
 
         template_values = {'video': video, 'collection': collection}
-        self.response.headers['Content-type'] = 'text/html'
+        # self.response.headers['Content-type'] = 'text/html'
         template = JINJA_ENVIRONMENT.get_template('Templates/drop.html')
         self.response.write(template.render(template_values))
 
-
-# class InstaHandler(webapp2.RequestHandler):
-#     def get(self):
-#         template = JINJA_ENVIRONMENT.get_template('Templates/drop.html')
-#         counter = 2
-#         url = ('https://api.instagram.com/v1/users/self/media/recent?'
-#             'access_token=145068709.1fb234f.d0a68e4a96fd44fba1b9082101de0e3b&count=%s' %(counter))
-#         collection = [ ]
-#         x=0
-#         for i in range(counter):
-#             logging.info("HEHEHHEHEHHEHE")
-#             string = urllib2.urlopen(url)
-#             bigdictionary = json.loads(string.read())
-#             picture = bigdictionary['data'][x]['link']
-#             collection += [picture]
-#             x += 1
-#         logging.info(collection)
-#         template_vars = {'collection':collection}
-#         self.response.write(template.render(template_vars))
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    # ('/insta', InstaHandler)
+    ('/drop', MainHandler),
+    ('/', LoginHandler)
 ], debug=True)
