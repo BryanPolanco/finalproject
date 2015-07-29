346a4950fe3b97e1ef7765402c2608894aba0621
shuying0814 <demouser@MacBook-Air-4.local>
2015-07-29 09:59:20 -0700

    Youtube

--- main.py
+++ main.py
@@ -14,17 +14,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 import webapp2
 import jinja2
 import os
+import urllib
+from google.appengine.api import urlfetch
+from apiclient.discovery import build
+from optparse import OptionParser
+import logging
+import random
+
+
+
+DEVELOPER_KEY = "AIzaSyAK_0glv6MteAUDzFCa9vd8q0d6ETePcw0"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja2_environment.get_template('templates/drop.html')
-        self.response.write(template.render())
+        youtube = build(
+            YOUTUBE_API_SERVICE_NAME,
+            YOUTUBE_API_VERSION,
+            developerKey=DEVELOPER_KEY)
+        search_response = youtube.search().list(
+            q = self.request.get('query'),
+            part="id,snippet",
+            maxResults = 50
+        ).execute()
 
-jinja2_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+        videos = []
 
+        for search_result in search_response.get("items", []):
+            videos.append(search_result)
+        randomindex = random.randint(0,len(videos)-1)
+        video = videos[randomindex]
+
+        template_values = {'video': video}
+        self.response.headers['Content-type'] = 'text/html'
+        template = JINJA_ENVIRONMENT.get_template('Templates/drop.html')
+        self.response.write(template.render(template_values))
+
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
 ], debug=True)
