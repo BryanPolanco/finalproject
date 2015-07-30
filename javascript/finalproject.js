73afbb463d73aff776fc62491dadf902c62fdfe1
Bryan Polanco <BryanPolanco@Bryans-MacBook-Pro.local>
2015-07-30 11:59:54 -0700

    update

--- javascript/finalproject.js
+++ javascript/finalproject.js
@@ -40,4 +40,36 @@ $("document").ready(function() {
 
        });
 
+       $('#instagram2').click(function(){
+
+       $('html, body').animate({
+         scrollTop: $("#instagram").offset().top
+       }, 1000);
+
+      });
+
+      $('#youtube2').click(function(){
+
+      $('html, body').animate({
+        scrollTop: $("#youtube").offset().top
+      }, 1000);
+
+     });
+
+     $('#twitter2').click(function(){
+
+     $('html, body').animate({
+       scrollTop: $("#twitter").offset().top
+     }, 1000);
+
+    });
+
+    $('#soundcloud2').click(function(){
+
+    $('html, body').animate({
+      scrollTop: $("#soundcloud").offset().top
+    }, 1000);
+
+   });
+
 });
